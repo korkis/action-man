2abd34059bb04de75713510a1481102e499783d1
Isu Kim <playkorkis@gmail.com>
2023-07-23 14:51:45 +0900

    init

---
+++ src/main/java/com/isu/actionman/ActionManApplication.java
@@ -0,0 +1,13 @@
+package com.isu.actionman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionManApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActionManApplication.class, args);
+	}
+
+}
