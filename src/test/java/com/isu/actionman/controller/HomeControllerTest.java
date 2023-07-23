933c72b85f0d97d740a082470cbdc9f88ce09bc9
Isu Kim <playkorkis@gmail.com>
2023-07-23 16:50:37 +0900

    mvn test

---
+++ src/test/java/com/isu/actionman/controller/HomeControllerTest.java
@@ -0,0 +1,14 @@
+package com.isu.actionman.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HomeControllerTest {
+
+    @Test
+    void test1() {
+        String str = "Junit is working fine";
+        assertEquals("Junit is working fine",str);
+    }
+}
