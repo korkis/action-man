28b822a4806b5fff1792f7ccdc6473194a402324
Isu Kim <playkorkis@gmail.com>
2023-07-23 15:17:40 +0900

    java 17

---
+++ action-man/src/test/java/com/isu/actionman/ActionManApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isu.actionman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionManApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
